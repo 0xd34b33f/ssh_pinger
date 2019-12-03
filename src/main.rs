14ce7ba6dee907496e766914e23f4856e816429b
0xd34db33f <0xd34db33f@noreply.github.com>
2019-12-03 16:24:23 +0300

    Fixed. Now works strange, but properly

--- src/main.rs
+++ src/main.rs
@@ -15,156 +15,167 @@ use rand::Rng;
 use rand::distributions::Alphanumeric;
 
 fn hosts_builder(path: String) -> Vec<String> {
-	let file = match File::open(std::path::Path::new(&path)) {
-		Ok(a) => a,
-		Err(e) => {
-			error!("Error opening hosts file: {}", e);
-			std::process::exit(1);
-		}
-	};
-	let f = BufReader::new(file);
-	let mut hosts = Vec::<String>::new();
-	for line in f.lines() {
-		let mut l = line.expect("Failed processing str as line");
-		if !l.chars().all(char::is_whitespace) {
-			l.push_str(":22");
-			hosts.push(l);
-		};
-	}
-	println!("len: {}", hosts.len());
-	hosts
+    let file = match File::open(std::path::Path::new(&path)) {
+        Ok(a) => a,
+        Err(e) => {
+            error!("Error opening hosts file: {}", e);
+            std::process::exit(1);
+        }
+    };
+    let f = BufReader::new(file);
+    let mut hosts = Vec::<String>::new();
+    for line in f.lines() {
+        let mut l = line.expect("Failed processing str as line");
+        if !l.chars().all(char::is_whitespace) {
+            l.push_str(":22");
+            hosts.push(l);
+        };
+    }
+    info!("config len: {}", hosts.len());
+    hosts
 }
 
-fn connect_host(host: String)
+fn connect_host(host: String) -> String
 {
-	let tcp = match TcpStream::connect(&host) {
-		Ok(a) => a,
-		Err(e) => {
-			warn!("Failed connected to {}:{}", &host, e);
-			return;
-		}
-	};
-	tcp.set_read_timeout(Some(Duration::new(1, 0))).unwrap();
-	let mut sess = match Session::new() {
-		Ok(a) => a,
-		Err(e) => {
-			warn!("Failed making session on {}:{}", &host, e);
-			return;
-		}
-	};
-	sess.set_tcp_stream(tcp);
-	match sess.handshake() {
-		Ok(_) => (),
-		Err(e) => {
-			warn!("Failed establishing handshake with {}:{}", &host, e);
-			return;
-		}
-	}
-	const LEN: usize = 10;
-	let user: String = rand::thread_rng()
-		.sample_iter(&Alphanumeric)
-		.take(LEN)
-		.collect();
-	let passwd: String = rand::thread_rng()
-		.sample_iter(&Alphanumeric)
-		.take(LEN)
-		.collect();
-	match sess.userauth_password(
-		user.as_str(),
-		passwd.as_str(),
-	) {
-		Ok(_) => (),
-		Err(e) => {
-			debug!("Done for: {}", &host);
-		}
-	}
+    info!("Started processing {}", &host);
+    let tcp = match TcpStream::connect(&host) {
+        Ok(a) => {
+            debug! {"TcpStream connected"};
+            a
+        }
+        Err(e) => {
+            // warn!("Failed connected to {}:{}", &host, e);
+            return format!("Failed connected to {}:{}", &host, e);
+        }
+    };
+    // tcp.set_read_timeout(Some(Duration::new(1, 0))).unwrap();
+    let mut sess = match Session::new() {
+        Ok(a) => a,
+        Err(e) => {
+            // warn!("Failed making session on {}:{}", &host, e);
+            return format!("Failed making session on {}:{}", &host, e);
+        }
+    };
+    sess.set_tcp_stream(tcp);
+    match sess.handshake() {
+        Ok(_) => (),
+        Err(e) => {
+            // warn!("Failed establishing handshake with {}:{}", &host, e);
+            return format!("Failed establishing handshake with {}:{}", &host, e);
+        }
+    }
+    const LEN: usize = 10;
+    let user: String = rand::thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(LEN)
+        .collect();
+    let passwd: String = rand::thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(LEN)
+        .collect();
+    match sess.userauth_password(
+        user.as_str(),
+        passwd.as_str(),
+    ) {
+        Ok(_) => (),
+        Err(e) => {
+            // debug!("Done for: {}", &host);
+        }
+    }
+    // debug!("Finished processing for {}", &host);
+    format!("Done for: {}", &host)
 }
 
 #[derive(Default)]
 struct Fool
-{
-}
+{}
 
 impl Worker for Fool {
-	type Input = String;
-	type Output = ();
-	
-	fn execute(&mut self, inp: Self::Input) -> Self::Output {
-		connect_host(inp)
-	}
+    type Input = String;
+    type Output = String;
+
+    fn execute(&mut self, inp: Self::Input) -> Self::Output {
+        connect_host(inp)
+    }
 }
 
 fn process_hosts(hosts: Vec<String>) {
-	info!("Started processing hosts");
-	let n_workers = 4;
-	let pool = Pool::<Fool>::new(n_workers);
-	let (tx, rx) = channel();
-	for host in hosts {
-		let host_name = host.clone();
-		pool.execute_to(tx.clone(), host_name);
-	}
+    info!("Started processing hosts");
+    let n_workers = 8;
+    debug!("Workers number: {}", n_workers);
+    let pool = Pool::<Fool>::new(n_workers);
+    let (tx, rx) = channel();
+    for host in &hosts {
+        let host_name = host.clone();
+        debug!("Started processing {}", &host_name);
+        pool.execute_to(tx.clone(), host_name);
+    }
+    let len = hosts.len();
+    for i in rx.iter() {
+        info!("{}", i);
+    }
 }
 
 fn main() {
-	let matches = App::new("Ssh pinger")
-		.version("0.1")
-		.arg(
-			Arg::with_name("path")
-				.short("p")
-				.long("path")
-				.value_name("PATH")
-				.help("Set path to the file with hostnames")
-				.required(true),
-		)
-		.get_matches();
-	let path = matches.value_of("path").unwrap().to_string();
-	set_up_logging();
-	let hosts = hosts_builder(path);
-	process_hosts(hosts);
+    let matches = App::new("Ssh pinger")
+        .version("0.1")
+        .arg(
+            Arg::with_name("path")
+                .short("p")
+                .long("path")
+                .value_name("PATH")
+                .help("Set path to the file with hostnames")
+                .required(true),
+        )
+        .get_matches();
+    let path = matches.value_of("path").unwrap().to_string();
+    set_up_logging();
+    let hosts = hosts_builder(path);
+    process_hosts(hosts);
 }
 
 fn set_up_logging() {
-	// configure colors for the whole line
-	let colors_line = ColoredLevelConfig::new()
-		.error(Color::Red)
-		.warn(Color::Yellow)
-		// we actually don't need to specify the color for debug and info, they are white by default
-		.info(Color::White)
-		.debug(Color::White)
-		// depending on the terminals color scheme, this is the same as the background color
-		.trace(Color::BrightBlack);
-	
-	// configure colors for the name of the level.
-	// since almost all of them are the some as the color for the whole line, we
-	// just clone `colors_line` and overwrite our changes
-	let colors_level = colors_line.clone().info(Color::Green);
-	// here we set up our fern Dispatch
-	fern::Dispatch::new()
-		.format(move |out, message, record| {
-			out.finish(format_args!(
-				"{color_line}[{date}][{target}][{level}{color_line}] {message}\x1B[0m",
-				color_line = format_args!(
-					"\x1B[{}m",
-					colors_line.get_color(&record.level()).to_fg_str()
-				),
-				date = chrono::Local::now().format("%Y-%m-%d %H:%M:%S"),
-				target = record.target(),
-				level = colors_level.color(record.level()),
-				message = message,
-			));
-		})
-		// set the default log level. to filter out verbose log messages from dependencies, set
-		// this to Warn and overwrite the log level for your crate.
-		.level(log::LevelFilter::Debug)
-		// change log levels for individual modules. Note: This looks for the record's target
-		// field which defaults to the module path but can be overwritten with the `target`
-		// parameter:
-		// `info!(target="special_target", "This log message is about special_target");`
-		.level_for("pretty_colored", log::LevelFilter::Trace)
-		// output to stdout
-		.chain(std::io::stdout())
-		.apply()
-		.unwrap();
-	
-	debug!("finished setting up logging! yay!");
+    // configure colors for the whole line
+    let colors_line = ColoredLevelConfig::new()
+        .error(Color::Red)
+        .warn(Color::Yellow)
+        // we actually don't need to specify the color for debug and info, they are white by default
+        .info(Color::White)
+        .debug(Color::White)
+        // depending on the terminals color scheme, this is the same as the background color
+        .trace(Color::BrightBlack);
+
+    // configure colors for the name of the level.
+    // since almost all of them are the some as the color for the whole line, we
+    // just clone `colors_line` and overwrite our changes
+    let colors_level = colors_line.clone().info(Color::Green);
+    // here we set up our fern Dispatch
+    fern::Dispatch::new()
+        .format(move |out, message, record| {
+            out.finish(format_args!(
+                "{color_line}[{date}][{target}][{level}{color_line}] {message}\x1B[0m",
+                color_line = format_args!(
+                    "\x1B[{}m",
+                    colors_line.get_color(&record.level()).to_fg_str()
+                ),
+                date = chrono::Local::now().format("%Y-%m-%d %H:%M:%S"),
+                target = record.target(),
+                level = colors_level.color(record.level()),
+                message = message,
+            ));
+        })
+        // set the default log level. to filter out verbose log messages from dependencies, set
+        // this to Warn and overwrite the log level for your crate.
+        .level(log::LevelFilter::Debug)
+        // change log levels for individual modules. Note: This looks for the record's target
+        // field which defaults to the module path but can be overwritten with the `target`
+        // parameter:
+        // `info!(target="special_target", "This log message is about special_target");`
+        .level_for("pretty_colored", log::LevelFilter::Trace)
+        // output to stdout
+        .chain(std::io::stdout())
+        .apply()
+        .unwrap();
+
+    debug!("finished setting up logging! yay!");
 }
